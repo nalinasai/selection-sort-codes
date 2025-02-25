2d41fb878f01d4bb6d6c3d98e3332413d9a24923
nalinasai <nalinanalina995@gmail.com>
2025-02-25 11:40:58 +0530

    add the java code for selection sort

--- selectionsort.java
+++ selectionsort.java
@@ -2,7 +2,36 @@ import java.lang.System;
 import java.util.Scanner;
 
 class selectionsort{
-    public static void main(){
-        
+    public static void main(String args[]){
+
+        int[] array = {2,1,5,3,7};
+        int n = array.length;
+
+        System.out.print("Unsorted list: ");
+
+        for (int element: array){
+            System.out.print(element+" ");
+        }
+
+        for(int i=0; i<n; i++){ // Outer loop to iterate through the entire array.
+            int temp = i;  // Assume the current element at index i is the smallest.
+            for(int j=i+1; j<n; j++){  // Inner loop to find the smallest element in the unsorted part of the array.
+                if(array[temp]>array[j]){
+                    temp = j;
+                }
+            }
+            //Swap the found smallest element with the element at position i
+            int temp_value = array[i];
+            array[i] = array[temp];
+            array[temp] = temp_value;
+            
+
+        }
+        System.out.println();
+
+        System.out.print("After sorting: ");
+        for (int element: array){
+            System.out.print(element+" ");
+        }
     }
 }
