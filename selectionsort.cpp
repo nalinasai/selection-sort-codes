642045e68984a9cdf9567f93640dc0fa1557158f
nalinasai <nalinanalina995@gmail.com>
2025-02-25 12:49:35 +0530

    modify the code for get the input from the user

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,16 +1,23 @@
 #include <iostream> //include the iostream library for the input output operations
+#include <vector>
+#include <sstream>
 using namespace std; // Use the standard namespace to avoid writing 'std::' before each standard function.
 
 int main(){
-    int unsorted_array[] = {2,1,5,4,3};
-    int n = size(unsorted_array);
-    cout << "Unsorted array: ";
-   
-    for (int i=0; i<5; i++){
-        cout << unsorted_array[i]<<" ";
-    }
-    cout<<endl;
+    
+    cout << "Enter the input: ";
+    vector<int> unsorted_array;
+    string inputs;
 
+    getline(cin, inputs);
+    stringstream ss(inputs);
+
+    int num;
+    while(ss>>num){
+        unsorted_array.push_back(num);
+    }
+
+    int n = unsorted_array.size();
     cout<< "Sorted array: ";
     for(int i=0; i<n; i++){ // Outer loop to iterate through the entire array.
         int temp =i;   // Assume the current element at index i is the smallest.
@@ -26,7 +33,7 @@ int main(){
         swap(unsorted_array[temp],unsorted_array[i]);
     }
 
-    for (int i=0; i<5; i++){
+    for (int i=0; i<unsorted_array.size(); i++){
         cout << unsorted_array[i]<<" ";
     }
 
