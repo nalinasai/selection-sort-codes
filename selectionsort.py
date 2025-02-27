b78c6431ca34f08d7e1db66e75e98a98cf731c38
nalinasai <nalinanalina995@gmail.com>
2025-02-27 22:23:35 +0530

    change the input method in python selection code

--- selectionsort.py
+++ selectionsort.py
@@ -1,24 +1,28 @@
-unsorted_array = [2,4,1,5,3]
-n = len(unsorted_array)
+#unsorted_array = [2,4,1,5,3]
 
-print("Before sorting: ")
-for element in unsorted_array:
-    print(element, end=" ")
 
-for i in range(n): # Iterate through each element in the list
+stringinput = input("Enter the input: ")
+
+integerlist = []
+stringsplit = stringinput.split()
+
+for i in stringsplit:
+    integerlist.append(int(i))
+
+for i in range(len(integerlist)): # Iterate through each element in the list
     temp = i  # Assume the current element is the smallest
-    for j in range(i+1,n):  # Iterate through the rest of the list
-        if(unsorted_array[temp]>unsorted_array[j]):
+    for j in range(i+1,len(integerlist)):  # Iterate through the rest of the list
+        if(integerlist[temp]>integerlist[j]):
             temp = j  # Update temp to index of the new smallest element
         else:
             continue
         
     # Swap the found smallest element with the element at position i
-    temp_value = unsorted_array[i]
-    unsorted_array[i] = unsorted_array[temp]
-    unsorted_array[temp] = temp_value
-print()
-print("After sorting: ")
-for element in unsorted_array:
+    temp_value = integerlist[i]
+    integerlist[i] = integerlist[temp]
+    integerlist[temp] = temp_value
+
+print("After sorting: ",end="")
+for element in integerlist:
     print(element, end=" ")
     
