c61fcdbfadd4b69ef8742be7a139df8d9a7e598f
nalinasai <nalinanalina995@gmail.com>
2025-02-27 21:49:08 +0530

    change the input method for java selection sort

--- selectionsort.java
+++ selectionsort.java
@@ -1,37 +1,44 @@
 import java.lang.System;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class selectionsort{
     public static void main(String args[]){
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter the input: ");
+        String stringinput = scan.nextLine();
+        String[] stringsplit = stringinput.split(" ");
 
-        int[] array = {2,1,5,3,7};
-        int n = array.length;
+        ArrayList<Integer> integerList = new ArrayList<>();
 
-        System.out.print("Unsorted list: ");
-
-        for (int element: array){
-            System.out.print(element+" ");
+        int n = stringsplit.length;
+        for (int k=0; k<n; k++){
+            int num = Integer.parseInt(stringsplit[k]);
+            integerList.add(num);
         }
-
+        
+        
         for(int i=0; i<n; i++){ // Outer loop to iterate through the entire array.
             int temp = i;  // Assume the current element at index i is the smallest.
             for(int j=i+1; j<n; j++){  // Inner loop to find the smallest element in the unsorted part of the array.
-                if(array[temp]>array[j]){
+                if(integerList.get(temp)>integerList.get(j)){
                     temp = j;
                 }
             }
             //Swap the found smallest element with the element at position i
-            int temp_value = array[i];
-            array[i] = array[temp];
-            array[temp] = temp_value;
+            int temp_value = integerList.get(i);
+            integerList.set(i,integerList.get(temp));
+            integerList.set(temp,temp_value);
             
 
         }
-        System.out.println();
+        
 
         System.out.print("After sorting: ");
-        for (int element: array){
-            System.out.print(element+" ");
+        for (int element: integerList){
+            System.out.print(element + " ");
         }
     }
 }
